5a388e1c7945936f5171c2b2ca29efb157399828
Dan Kim <itsdanya@gmail.com>
2024-02-29 00:10:12 +0300

    Add TMDB IDs to movies in data JSON file

--- match_tmdb.rb
+++ match_tmdb.rb
@@ -1,29 +1,36 @@
 require 'json'
+require 'dotenv'
+require 'themoviedb'
 
 # Read data from data.json
 file = File.read('data.json')
 data = JSON.parse(file)
 
+Dotenv.load
+Tmdb::Api.key(ENV['THEMOVIEDB_API_KEY'])
+
 # Iterate through each item
 data.each do |item|
-  # title = item['title']
-  # year = item['year']
+  title = item['title']
+  year = item['year']
 
   # # Search TMDB for the query
-  # query = "#{title} #{year}"
-  # tmdb_id = search_tmdb(query)
+  search = Tmdb::Search.new
+  search.resource('movie')
+  search.query(title)
+  search.year(year)
+  result = search.fetch
+
+  next unless result.any?
+
+  # # Get the TMDB ID from the result
+  tmdb_id = result.first['id']
 
   # # Assign TMDB ID to the item
-  # item['tmdb_id'] = tmdb_id
+  item['tmdb_id'] = tmdb_id
 end
 
 # Save the updated data back to data.json
 File.open('data_with_tmdb.json', 'w') do |file|
   file.write(data.to_json)
-end
-
-def search_tmdb(query)
-  # Perform the search on TMDB
-  # Replace this with your actual TMDB search implementation
-  # and return the TMDB ID
 end
