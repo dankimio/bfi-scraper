8cee8016c85a9b66a96fc45a09eec457a17aae40
Dan Kim <itsdanya@gmail.com>
2024-02-28 23:10:59 +0300

    Refactor item processing loop in BFI class

--- main.rb
+++ main.rb
@@ -21,13 +21,32 @@ class BFI
     find('.calendar-container').find_button('29').click
 
     events = []
-    all('.item-name').each do |item|
-      title = item.find('a.more-info')['title'] rescue nil
+    all('.item-name')[0..2].each do |item|
+      url = item.find('a.more-info')['href']
+      # new_window = window_opened_by { item.find('a.more-info').click }
 
-      result = { title: title }
-      puts result
+      within_window(open_new_window) do
+        visit(url)
+        title = find('h1').text
 
-      events << result
+        values = all('.Film-info__information__value')
+          .map(&:text)
+          .join
+
+        match = values.match(/\d{4}/)
+
+        if match
+          matched_year = match[0].to_i
+
+          if (1900..2030).include?(matched_year)
+            year = matched_year
+          end
+        end
+
+        next unless year
+
+        puts events << { title: title, year: year }
+      end
     end
   end
 end
