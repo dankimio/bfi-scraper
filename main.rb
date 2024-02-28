a4ebfa8671322cfd28ee4baca122f6984218fe5d
Dan Kim <itsdanya@gmail.com>
2024-02-28 20:06:55 +0300

    Update Capybara driver configuration for Selenium

--- main.rb
+++ main.rb
@@ -3,7 +3,9 @@ require 'selenium-webdriver'
 
 BASE_URL = 'https://whatson.bfi.org.uk'
 
-Capybara.default_driver = :selenium_chrome_headless
+Capybara.default_driver = :selenium
+# Capybara.default_driver = :selenium_chrome
+# Capybara.default_driver = :selenium_chrome_headless
 Capybara.app_host = BASE_URL
 
 class BFI
@@ -15,14 +17,17 @@ class BFI
   def run
     visit('/Online/default.asp')
 
-    events = []
-    all('.event-list-item').each do |item| # Adjust the selector as per actual HTML structure
-      title = item.find('.event-title').text rescue nil # Adjust the selector
-      date = item.find('.event-date').text rescue nil # Adjust the selector
-      link = item.find('.buy-link')['href'] rescue nil # Adjust the selector
+    # Click on .calendar-day>button with contents "29"
+    find('.calendar-day > button', text: '29').click
 
-      events << { title: title, date: date, link: link }
-    end
+    # events = []
+    # all('.event-list-item').each do |item| # Adjust the selector as per actual HTML structure
+    #   title = item.find('.event-title').text rescue nil # Adjust the selector
+    #   date = item.find('.event-date').text rescue nil # Adjust the selector
+    #   link = item.find('.buy-link')['href'] rescue nil # Adjust the selector
+
+    #   events << { title: title, date: date, link: link }
+    # end
   end
 end
 
